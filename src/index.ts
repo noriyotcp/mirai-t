2e032e2ddbb0449779f45c6bb58730e71a8d4a8b
noriyotcp <noriyo.akita@gmail.com>
2019-09-14 21:13:27 +0900

    FizzBuzz
    This is a practice swing.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-const greet = (message = "World") => console.log(`Hello ${message}!`);
-greet("Boilerplate");
+const argNum : number = Number(process.argv[2]);
+
+function fizzbuzz(num : number) : string {
+  if (num % 15 === 0) {
+    return "FizzBuzz";
+  } else if (num % 3 === 0) {
+    return "Fizz";
+  } else if (num % 5 === 0) {
+    return "Buzz";
+  }
+  return num.toString();
+}
+
+console.log(fizzbuzz(argNum));
