4d418a791c63b1e53abc771285ed2dea60925688
noriyotcp <noriyo.akita@gmail.com>
2019-09-16 21:09:46 +0900

    Add a practice swing to foo.test.ts
    Use jest.spyOn

--- src/foo.ts
+++ src/foo.ts
@@ -1,1 +1,4 @@
 export const sum = (...a: number[]) => a.reduce((acc, val) => acc + val, 0);
+export const printSum = (...a: number[]) => {
+  console.log(sum(...a));
+};
--- test/foo.test.ts
+++ test/foo.test.ts
@@ -1,4 +1,4 @@
-import { sum } from "../src/foo";
+import { sum, printSum } from "../src/foo";
 
 // test("basic", () => {
 //   expect(sub()).toBe(0);
@@ -6,4 +6,15 @@ import { sum } from "../src/foo";
 
 test("basic again", () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+const spyLog = jest.spyOn(console, "log");
+spyLog.mockImplementation(x => x);
+
+test("printSum", () => {
+  printSum(1, 2);
+  printSum(3, 4);
+  expect(console.log).toBeCalled();
+  expect(spyLog.mock.calls[0][0]).toEqual(3);
+  expect(spyLog.mock.calls[1][0]).toEqual(7);
 });
